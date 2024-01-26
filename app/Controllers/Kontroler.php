28b23f93fa87e91b369818235c7feffe2c43da9a
Projekt_druhy_rocnik <jan.kalisz007@gmail.com>
2024-01-26 11:34:33 +0100

    j

--- app/Controllers/Kontroler.php
+++ app/Controllers/Kontroler.php
@@ -8,6 +8,8 @@ use App\Models\SpolkZeme;
 class Kontroler extends BaseController
 {
     var $spolkZeme;
+    var $Station;
+
     public function __construct(){
         $this->spolkZeme = new spolkZeme();
     }
@@ -19,6 +21,12 @@ class Kontroler extends BaseController
 
     public function getIndividualBundesland($idbundesland){
         $data['spolkZeme'] = $this->spolkZeme->find($idbundesland);
+        echo view('kartaSpolkZeme', $data);
+    }
+
+    public function getStations($idbundesland){
+        $data['Station'] = $this->Station->where('bundesland', $idbundesland)->FindAll();
+        $data['name'] = $this->spolkZeme->find($idbundesland)->name;
         echo view('kartaSpolkZeme', $data);
     }
 }
--- app/Views/kartaSpolkZeme.php
+++ app/Views/kartaSpolkZeme.php
@@ -3,7 +3,19 @@
 <?=$this->section("content");?>
 
 <div class="container-fluid">
+    <h1>Přehled meteorologických stanic ve spolkové zemi - <?= $name ?> </h1>
+    <div class = "row">
+        <?php 
+            //sem dám foreach cyklus
+        ?>
+        <div class = "col-12">
+            <div class="card">
+                <div class="card-body">
 
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?=$this->endSection();?>
